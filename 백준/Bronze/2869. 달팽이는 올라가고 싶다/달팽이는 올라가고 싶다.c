17cf0115cb33f5cbfa9af7cecae67bfe5a88c846
SeungHeon3649 <94602281+SeungHeon3649@users.noreply.github.com>
2024-04-03 15:10:51 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A, B, V;
+	int day = 0;
+	scanf("%d %d %d", &A, &B, &V);
+	day = (V - B - 1) / (A - B) + 1;
+	printf("%d", day);
+	return 0;
+}
